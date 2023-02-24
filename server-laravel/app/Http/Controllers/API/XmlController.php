c86492746eb5eec5634d6865840e04ba8137ab91
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-02-24 14:10:44 +0100

    creation of models and functions for controllers

--- server-laravel/app/Http/Controllers/API/XmlController.php
+++ server-laravel/app/Http/Controllers/API/XmlController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Gallery;
+use App\Models\Item;
+use App\Models\ItemCate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class XmlController extends Controller
 {
