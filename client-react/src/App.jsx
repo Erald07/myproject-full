9673584b0a19cc6d8f3075c2f68139a05260af48
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-02-25 13:32:13 +0100

    creating route for registration component

--- client-react/src/App.jsx
+++ client-react/src/App.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ReadXml from './components/xml/ReadXmlFile';
 import Home from './components/Home';
 import Login from './components/authentication/Login';
+import Register from './components/authentication/Register';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Route exact path='/' element={<Home />} />
                 <Route exact path='/read-xml' element={<ReadXml />} />
                 <Route exact path='/login' element={<Login/>} />
+                <Route exact path='/register' element={<Register/>} />
             </Routes>
         </Router>
     );
