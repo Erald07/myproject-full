11e6ab2a37f15a05347ac99ad63430a8d3cd2e7b
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-02-25 13:35:22 +0100

    creating route api for authentication

--- client-react/src/App.jsx
+++ client-react/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import axios from 'axios';
 
 import ReadXml from './components/xml/ReadXmlFile';
 import Home from './components/Home';
 import Login from './components/authentication/Login';
 import Register from './components/authentication/Register';
+
+axios.interceptors.request.use(function(config){
+    const token = localStorage.getItem('auth_token');
+    config.headers.Authorization = token ? `Bearer ${token}` : '';
+    return config;
+});
 
 function App() {
     return (
--- server-laravel/routes/api.php
+++ server-laravel/routes/api.php
@@ -4,10 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\XmlController;
 use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\UserController;
 
 Route::post('/read-xml', [XmlController::class, 'index']);
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/{subName}', [CategoryController::class, 'subcategory']);
+
+Route::post('/register-user', [UserController::class, 'registerUser']);
+Route::post('/login-user', [UserController::class, 'loginUser']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
