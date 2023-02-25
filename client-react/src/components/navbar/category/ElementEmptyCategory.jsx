01cf4515e6b09bc3cb5805225d6f0695bc7447ec
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-02-25 22:07:54 +0100

    creating file for empty element category

---
+++ client-react/src/components/navbar/category/ElementEmptyCategory.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
+const ElementEmptyCategory = props => {
+    return(
+        <li {...props.key} className="nav-item border-gray-300 border-t">
+            <Link className="nav-links py-5 w-full flex justify-between font-semibold px-6" to={props.link}>
+                <div className="lowercase first-letter:capitalize" >
+                    {props.name}
+                </div>
+                {props.isEmpty ? '' : <FontAwesomeIcon icon={faAngleRight} className="text-primary float-right py-1" />}
+            </Link>
+        </li>
+    );
+}
+
+export default ElementEmptyCategory;
