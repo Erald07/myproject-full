84be08bb17ab9ba22111e48793d5893faa2723fe
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-02-28 10:29:53 +0100

    including the header in the index1 component

--- client-react/src/components/items/Index1.jsx
+++ client-react/src/components/items/Index1.jsx
@@ -1,25 +1,40 @@
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
+import Header from "./header/Header";
 import Items from "./Items";
 import "./Style.css";
 
 function Index1(){
     const [items, setItems] = useState([]);
-
+    console.log(items);
     const {getcate} = useParams();
 
     useEffect(() => {
-        fetch(`http://localhost:8000/api/categoria-prodotto/${getcate}`)
-          .then(items => items.json())
-          .then(
-                (result) => {
-                    setItems(result.items);
-                },
-            )
+        // fetch(`http://localhost:8000/api/categoria-prodotto/${getcate}`)
+        //   .then(items => items.json())
+        //   .then(
+        //         (result) => {
+        //             setItems(result.items);
+        //         },
+        //     )
+        const getSubCate = async () => {
+            const result = await fetch(`http://localhost:8000/api/categoria-prodotto/${getcate}`);
+            const subcates = await result.json();
+            // console.log(subcates);
+            // if(subcates.status === 200){
+                setItems(subcates.items);
+            // }
+        }
+
+        getSubCate();
+        // getItems();
     },[getcate]);
     
     return(
         <>
+        <div>
+            <Header data={items}/>
+        </div>
         <div>
             <Items data={items} />
         </div>
