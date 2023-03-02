b289946454be73c7d7b0634fd0066cd2b31302fb
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-03-02 09:47:45 +0100

    creating component MarcheFilter

---
+++ client-react/src/components/items/filters/MarcheFilter.jsx
@@ -0,0 +1,41 @@
+import React, {useContext} from 'react'
+import { ContextFilter } from './contextFilter/ContextFilter';
+
+export default function StockFilter(props) {
+    
+    const {isShow3, data} = useContext(ContextFilter);
+
+    function filterDuplicatesMarche(arr){
+        return arr.filter((item, index) => arr.findIndex(data => data.marche === item.marche) === index);
+    }
+
+    return (
+        <div>
+        {isShow3 ?
+        <div className='container overflow-y-auto shadow-md'>
+            <div className='pt-5 px-5'>
+                <p className='pb-6 font-medium text-primary text-lg'>Filtra per marca</p>
+            </div>
+            <div className='grid grid-cols-5 gap-y-4 px-5 pb-2'>
+            {filterDuplicatesMarche(data).map((item, j) => {
+                // console.log(item.marche);
+                return(
+                <div className='items-center'>
+                    <div key={j} className='flex items-center'>
+                        <input type='checkbox' className={`accent-secondary w-6 h-6`} />
+                        <p className='flex ml-5'>{item.marche}</p>
+                    </div>
+                </div>
+                );
+            })}
+            </div>
+            <div className='px-5 py-3 mt-3 border-t border-solid border-gray-100'>
+                <button className='uppercase bg-primary text-white py-1 px-8 text-center rounded-full'>Applica</button>
+            </div>
+        </div>
+        :
+        ""
+        }
+        </div>
+    );
+}
