80e34bde5d608fac66e2230fbd8db7b82f31ea0c
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-03-16 13:42:06 +0100

    categoryDesktop component update

--- client-react/src/components/navbar/category/CategoryDesktop.jsx
+++ client-react/src/components/navbar/category/CategoryDesktop.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate, Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretUp, faCircleInfo } from "@fortawesome/free-solid-svg-icons";
@@ -6,75 +6,79 @@ import ButonCategory from "../ButonCategory";
 import DropdownCategory from "../DropdownCategory";
 import ImageDropdown from '../ImageDropdown';
 import { CategoryContext } from '../contextCategory/CategoryContext';
+import CategoryMobile from './CategoryMobile';
+import Footer from '../../items/elements/Footer';
 
-const App = () => {
+const CategoryDesktop = () => {
+    const {state, getCategory, getSubCategory, count} = useContext(CategoryContext);
+    // const [count, setCount] = useState(0);
+    // const [state, setState] = useState([]);
+    // const [getCategory, setCategory] = useState();
+    // const [getSubCategory, setSubCategory] = useState([]);
+    // const [getImage, setImage] = useState([]);
+    // const [isShown, setIsShown] = useState(false);
 
-    const [count, setCount] = useState(0);
-    const [state, setState] = useState([]);
-    const [getCategory, setCategory] = useState();
-    const [getSubCategory, setSubCategory] = useState([]);
-    const [getImage, setImage] = useState([]);
-    const [isShown, setIsShown] = useState(false);
 
     const navigate = useNavigate();
 
-    useEffect(() => {
-        const getCate = async () => {
-            const result = await fetch('http://localhost:8000/api/categories');
-            const cates = await result.json();
-            // console.log(cates.categories);
-            if(cates.status === 200){
-                setState(cates.categories);
-            }
-        }
+    // useEffect(() => {
+    //     const getCate = async () => {
+    //         const result = await fetch('http://localhost:8000/api/categories');
+    //         const cates = await result.json();
+    //         // console.log(cates.categories);
+    //         if(cates.status === 200){
+    //             setState(cates.categories);
+    //         }
+    //     }
 
-        getCate();
-    }, []);
+    //     getCate();
+    // }, []);
 
-    useEffect(()=> {
-        const getSubCate = async () => {
-            const result = await fetch(`http://localhost:8000/api/categories/${getCategory}`);
-            const subcates = await result.json();
-            if(subcates.status === 200){
-                setSubCategory(subcates.subcategory);
-                setImage(subcates.subimage);
-            }
-        }
+    // useEffect(()=> {
+    //     const getSubCate = async () => {
+    //         const result = await fetch(`http://localhost:8000/api/categories/${getCategory}`);
+    //         const subcates = await result.json();
+    //         if(subcates.status === 200){
+    //             setSubCategory(subcates.subcategory);
+    //             setImage(subcates.subimage);
+    //         }
+    //     }
 
-        getSubCate();
+    //     getSubCate();
 
-    },[getCategory],[getImage]);
+    // },[getCategory],[getImage]);
 
-    var subcategory = "";
-    subcategory = getSubCategory[0]?.subcategory?.map( (sub1, i) => {
-        return(
-            <div className='pl-10 py-8 space-y-3'>
-                <Link to={`categoria-prodotto/${getCategory}/${sub1.cate_name}`} key={i} className='mb-3 uppercase text-md font-bold'>{sub1.cate_name}</Link>
-                <div className='py-2'>
-                    {sub1.subcategory.map( (sub2, j) => {
-                        return(
-                            <>
-                            <Link to={`categoria-prodotto/${getCategory}/${sub1.cate_name}/${sub2.cate_name}`} key={j} className='text-sm py-2'>{sub2.cate_name}</Link>
-                            <div className='py-2'>
-                                {sub2?.subcategory?.map( (sub3, k) => {
-                                    return(
-                                        <Link to={`categoria-prodotto/${getCategory}/${sub1.cate_name}/${sub2.cate_name}/${sub3.cate_name}`} key={k} className='text-sm pl-3'>{sub3.cate_name}</Link>
-                                    );
-                                })}
-                            </div>
-                            </>
-                        );
-                    })}
-                </div>
-            </div>
-        );
-    });
+    // var subcategory = "";
+
+    // subcategory = getSubCategory[0]?.subcategory?.map( (sub1, i) => {
+    //     return(
+    //         <div key={i} className='pl-10 py-8 space-y-3'>
+    //             <Link to={`categoria-prodotto/${getCategory}/${sub1.cate_name}`} key={i} className='mb-3 uppercase text-md font-bold'>{sub1.cate_name}</Link>
+    //             <div className='py-2'>
+    //                 {sub1.subcategory.map( (sub2, j) => {
+    //                     return(
+    //                         <>
+    //                         <Link to={`categoria-prodotto/${getCategory}/${sub1.cate_name}/${sub2.cate_name}`} key={j} className='text-sm py-2'>{sub2.cate_name}</Link>
+    //                         <div className='py-2'>
+    //                             {sub2?.subcategory?.map( (sub3, k) => {
+    //                                 return(
+    //                                     <Link to={`categoria-prodotto/${getCategory}/${sub1.cate_name}/${sub2.cate_name}/${sub3.cate_name}`} key={k} className='text-sm pl-3'>{sub3.cate_name}</Link>
+    //                                 );
+    //                             })}
+    //                         </div>
+    //                         </>
+    //                     );
+    //                 })}
+    //             </div>
+    //         </div>
+    //     );
+    // });
 
     var categoriesDesktop = "";
 
     categoriesDesktop = state?.map((category, p) => {
         return(
-            <div className="hidden lg:relative lg:inline-flex lg:items-center lg:px-4">
+            <div key={p} className="hidden lg:relative lg:inline-flex lg:items-center lg:px-4">
                 <div>
                 <ButonCategory 
                     key={p}
@@ -88,15 +92,18 @@ const App = () => {
     });
 
     return( 
-        <CategoryContext.Provider value={{count, setCount, getSubCategory, setSubCategory, setIsShown, setCategory, subcategory, getImage}}>
+        // <CategoryContext.Provider value={{count, setCount, getSubCategory, setSubCategory, setIsShown, setCategory, subcategory, getImage, state}}>
+        <>
         <div className="container mb-1">
             {categoriesDesktop} 
         </div>
         <div className='hidden lg:block border border-gray-200 border-solid'></div>
         {count === 1 ? <DropdownCategory /> : ""}
         {count === 2 ? navigate(`/categoria-prodotto/${getCategory}`) : ""}
-
-        </CategoryContext.Provider>
+        {/* <CategoryMobile /> */}
+        {/* <Footer /> */}
+        </>
+        // </CategoryContext.Provider>
     );
 }
-export default App;
+export default CategoryDesktop;
