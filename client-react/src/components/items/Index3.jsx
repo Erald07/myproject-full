d9aba5ee1f5141cfe252817d87244687a661078e
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-03-16 13:07:38 +0100

    update component Index3

--- client-react/src/components/items/Index3.jsx
+++ client-react/src/components/items/Index3.jsx
@@ -11,29 +11,28 @@ function Index3(){
     const {getsubcate} = useParams();
     const {getsubsubcate} = useParams();
 
-    useEffect(() => {
-        fetch(`http://localhost:8000/api/categoria-prodotto/${getcate}/${getsubcate}/${getsubsubcate}`)
-          .then(items => items.json())
-          .then(
-                (result) => {
-                    // console.log(result);
-                    setItems(result.items);
-                },
-            )
+    // useEffect(() => {
+    //     fetch(`http://localhost:8000/api/categoria-prodotto/${getcate}/${getsubcate}/${getsubsubcate}`)
+    //       .then(items => items.json())
+    //       .then(
+    //             (result) => {
+    //                 // console.log(result);
+    //                 setItems(result.items);
+    //             },
+    //         )
+    // },[getcate, getsubcate, getsubsubcate]);
+    useEffect(()=> {
+        const getSubSubSubCate = async () => {
+            const result = await fetch(`http://localhost:8000/api/categoria-prodotto/${getcate}/${getsubcate}/${getsubsubcate}`);
+            const subcates = await result.json();
+            if(subcates.status === 200){
+                setItems(subcates.items);
+            }
+        }
+
+        getSubSubSubCate();
+
     },[getcate, getsubcate, getsubsubcate]);
-    // useEffect(()=> {
-    //     const getSubCate = async () => {
-    //         const result = await fetch(`http://localhost:8000/api/categoria-prodotto/${getcate}/${getsubcate}`);
-    //         const subcates = await result.json();
-    //         console.log(subcates);
-    //         if(subcates.status === 200){
-    //             setItems(subcates.items);
-    //         }
-    //     }
-
-    //     getSubCate();
-
-    // },[getcate, getsubcate]);
     
     return(
         <>
