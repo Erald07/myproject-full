9c78b267a83a7c35f5b9096b3a0e824da71c3651
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-03-25 13:55:18 +0100

    modal for genere filters

--- client-react/src/components/items/filters/GenereFilter.jsx
+++ client-react/src/components/items/filters/GenereFilter.jsx
@@ -1,12 +1,71 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
+import { Link } from 'react-router-dom';
 import { ContextFilter } from './contextFilter/ContextFilter';
 
-export default function StockFilter(props) {
+export default function GenereFilter(props) {
     
-    const {isShow4, data} = useContext(ContextFilter);
+    const {isShow4, data, setIsShow4} = useContext(ContextFilter);
+    const [genereInfo, setGenereInfo] = useState({
+        genere: [],
+    });
 
     function filterDuplicatesGenere(arr){
         return arr.filter((item, index) => arr.findIndex(data => data.genere === item.genere) === index);
+    }
+
+    const handleInputChange = (e) => {
+        const { value, checked } = e.target;
+        const { genere } = genereInfo;
+          
+        if (checked) {
+            setGenereInfo({
+                genere: [...genere, value],
+            });
+        }
+      
+        else {
+            setGenereInfo({
+                genere: genere.filter((e) => e !== value),
+            });
+        }
+    };
+
+    const queryParams = new URLSearchParams(window.location.search);
+    var colore = '';
+    var marche = '';
+    var eta = '';
+    var taglia = '';
+    var availability = '';
+    var min_price = '';
+    var max_price = '';
+    var query = `?genere=${genereInfo['genere'].toString()}`;
+    if(queryParams.get("marche") !== null){
+        marche = queryParams.get("marche");
+        query += `&marche=${marche}`;
+    }
+    if(queryParams.get("eta") !== null){
+        eta = queryParams.get("eta");
+        query += `&eta=${eta}`;
+    }
+    if(queryParams.get("colore") !== null){
+        colore = queryParams.get("colore");
+        query += `&colore=${colore}`;
+    }
+    if(queryParams.get("exclude_out_of_stock") !== null){
+        availability = queryParams.get("exclude_out_of_stock");
+        query += `&exclude_out_of_stock=${availability}`;
+    }
+    if(queryParams.get("min_price") !== null){
+        min_price = queryParams.get("min_price");
+        query += `&min_price=${min_price}`;
+    }
+    if(queryParams.get("max_price") !== null){
+        max_price = queryParams.get("max_price");
+        query += `&max_price=${max_price}`;
+    }
+    if(queryParams.get("taglia") !== null){
+        taglia = queryParams.get("taglia");
+        query += `&taglia=${taglia}`;
     }
 
     return (
@@ -17,13 +76,19 @@ export default function StockFilter(props) {
                 <p className='pb-6 font-medium text-primary text-lg'>Filtra per sesso</p>
             </div>
             <div className='grid grid-cols-5 gap-y-4 px-5 pb-2'>
-            {filterDuplicatesGenere(data).map((item, k) => {
+            {filterDuplicatesGenere(data).map((item, i) => {
                 return(
-                <div className='items-center'>
+                <div key={i} className='items-center'>
                     {item.genere !== "NULL" ? 
-                    <div key={k} className='flex items-center'>
-                        <input type='checkbox' className={`accent-secondary w-6 h-6`} />
-                        <p className='flex ml-5'>{item.genere}</p>
+                    <div className='flex items-center'>
+                        {filterDuplicatesGenere(item.genere.split(',')).map((val, j) => {
+                            return(                                
+                                <div key={j} className='inline-flex'>
+                                    <input type='checkbox' value={val} className={`accent-secondary w-6 h-6`} onChange={handleInputChange} />
+                                    <p className='flex ml-5'>{val}</p>
+                                </div>
+                            );
+                        })}
                     </div>
                     :
                     ""
@@ -33,7 +98,7 @@ export default function StockFilter(props) {
             })}
             </div>
             <div className='px-5 py-3 mt-3 border-t border-solid border-gray-100'>
-                <button className='uppercase bg-primary text-white py-1 px-8 text-center rounded-full'>Applica</button>
+                <Link to={query} onClick={() => setIsShow4(false)} className='uppercase bg-primary text-white py-1 px-8 text-center rounded-full'>Applica</Link>
             </div>
         </div>
         :
