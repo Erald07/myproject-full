bb51a688ac85def153664d9b72cfbfd782b8355d
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-03-25 14:22:02 +0100

    merging all filters

---
+++ server-laravel/app/Filters/ItemFilter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filters;
+
+use App\Filters\AbstractFilter;
+use Illuminate\Database\Eloquent\Builder;
+
+class ItemFilter extends AbstractFilter
+{
+    protected $filters = [
+        'colore' => ColoreFilter::class,
+        'genere' => GenereFilter::class,
+        'marche' => MarcheFilter::class,
+        'exclude_out_of_stock' => StockPriceFilter::class,
+        'min_price' => MinPriceFilter::class,
+        'max_price' => MaxPriceFilter::class,
+        'orderBy' => PriceFilter::class,
+        'eta' => EtaFilter::class,
+        'taglia' => TagliaFilter::class,
+    ];
+}
