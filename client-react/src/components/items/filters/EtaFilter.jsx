c9d4024e5b0b8a8438d3b8b1fc09bf92792e8dc1
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-03-25 13:54:37 +0100

    modal for eta filters

--- client-react/src/components/items/filters/EtaFilter.jsx
+++ client-react/src/components/items/filters/EtaFilter.jsx
@@ -1,12 +1,71 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
+import { Link } from 'react-router-dom';
 import { ContextFilter } from './contextFilter/ContextFilter';
 
-export default function StockFilter(props) {
+export default function EtaFilter(props) {
     
-    const {isShow5, data} = useContext(ContextFilter);
+    const {isShow5, data, setIsShow5} = useContext(ContextFilter);
+    const [etaInfo, setEtaInfo] = useState({
+        eta: [],
+    });
 
     function filterDuplicatesEta(arr){
         return arr.filter((item, index) => arr.findIndex(data => data.eta === item.eta) === index);
+    }
+
+    const handleInputChange = (e) => {
+        const { value, checked } = e.target;
+        const { eta } = etaInfo;
+          
+        if (checked) {
+            setEtaInfo({
+                eta: [...eta, value],
+            });
+        }
+      
+        else {
+            setEtaInfo({
+                eta: eta.filter((e) => e !== value),
+            });
+        }
+    };
+
+    const queryParams = new URLSearchParams(window.location.search);
+    var colore = '';
+    var marche = '';
+    var genere = '';
+    var taglia = '';
+    var availability = '';
+    var min_price = '';
+    var max_price = '';
+    var query = `?eta=${etaInfo['eta'].toString()}`;
+    if(queryParams.get("marche") !== null){
+        marche = queryParams.get("marche");
+        query += `&marche=${marche}`;
+    }
+    if(queryParams.get("genere") !== null){
+        genere = queryParams.get("genere");
+        query += `&genere=${genere}`;
+    }
+    if(queryParams.get("colore") !== null){
+        colore = queryParams.get("colore");
+        query += `&colore=${colore}`;
+    }
+    if(queryParams.get("exclude_out_of_stock") !== null){
+        availability = queryParams.get("exclude_out_of_stock");
+        query += `&exclude_out_of_stock=${availability}`;
+    }
+    if(queryParams.get("min_price") !== null){
+        min_price = queryParams.get("min_price");
+        query += `&min_price=${min_price}`;
+    }
+    if(queryParams.get("max_price") !== null){
+        max_price = queryParams.get("max_price");
+        query += `&max_price=${max_price}`;
+    }
+    if(queryParams.get("taglia") !== null){
+        taglia = queryParams.get("taglia");
+        query += `&taglia=${taglia}`;
     }
 
     return (
@@ -17,13 +76,19 @@ export default function StockFilter(props) {
                 <p className='pb-6 font-medium text-primary text-lg'>Età</p>
             </div>
             <div className='grid grid-cols-5 gap-y-4 px-5 pb-2'>
-            {filterDuplicatesEta(data).map((item, k) => {
+            {filterDuplicatesEta(data).map((item, i) => {
                 return(
-                <div className='items-center'>
+                <div key={i} className='items-center'>
                     {item.eta !== "NULL" ? 
-                    <div key={k} className='flex items-center'>
-                        <input type='checkbox' className={`accent-secondary w-6 h-6`} />
-                        <p className='flex ml-5'>{item.eta}</p>
+                    <div className='flex items-center'>
+                        {filterDuplicatesEta(item.eta.split(',')).map((val, j) => {
+                            return(                                
+                                <div key={j} className='inline-flex'>
+                                    <input type='checkbox' value={val} className={`accent-secondary w-6 h-6`} onChange={handleInputChange} />
+                                    <p className='flex ml-5'>{val}</p>
+                                </div>
+                            );
+                        })}
                     </div>
                     :
                     ''
@@ -33,7 +98,7 @@ export default function StockFilter(props) {
             })}
             </div>
             <div className='px-5 py-3 mt-3 border-t border-solid border-gray-100'>
-                <button className='uppercase bg-primary text-white py-1 px-8 text-center rounded-full'>Applica</button>
+                <Link to={query} onClick={() => setIsShow5(false)} className='uppercase bg-primary text-white py-1 px-8 text-center rounded-full'>Applica</Link>
             </div>
         </div>
         :
