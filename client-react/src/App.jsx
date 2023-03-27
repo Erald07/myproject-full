b0278e406cf6e4f7ff13ffc4af97f76a7407dd04
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-03-27 22:50:41 +0200

    adding routes

--- client-react/src/App.jsx
+++ client-react/src/App.jsx
@@ -10,6 +10,15 @@ import Index1 from './components/items/Index1';
 import Index2 from './components/items/Index2';
 import Index3 from './components/items/Index3';
 import Index4 from './components/items/Index4';
+import Context from './components/navbar/contextCategory/Context';
+import Footer from './components/items/elements/Footer';
+import FilterFooter from './components/items/elements/FilterFooter';
+import Item from './components/items/item/Item';
+import CartProvider from './components/items/cart/ContextCart';
+import Items from './components/items/Items';
+import Cart from './components/items/cart/Cart';
+import ModalSearch from './components/navbar/modalSearch/ModalSearch';
+import QueryFilter from './components/items/filters/contextFilter/ContextFilter';
 
 axios.interceptors.request.use(function(config){
     const token = localStorage.getItem('auth_token');
@@ -20,7 +29,9 @@ axios.interceptors.request.use(function(config){
 function App() {
     return (
         <Router>
-            <Navbar />
+            <CartProvider>
+                <Navbar />
+            </CartProvider>
             <Routes>
                 <Route exact path='/' element={<Index1/>} />
                 <Route exact path='/read-xml' element={<ReadXml />} />
@@ -30,6 +41,15 @@ function App() {
                 <Route exact path='/categoria-prodotto/:getcate/:getsubcate' element={<Index2/>} />
                 <Route exact path='/categoria-prodotto/:getcate/:getsubcate/:getsubsubcate' element={<Index3/>} />
                 <Route exact path='/categoria-prodotto/:getcate/:getsubcate/:getsubsubcate/:getsubsubsubcate' element={<Index4/>} />
+                <Route exact path='/prodotto/:getTitle' element={<Item />} />
+                <Route exact path='modal-search' element={<ModalSearch />} />
+            </Routes>
+            <Context>
+                <FilterFooter />
+                <Footer />
+            </Context>
+            <Routes>
+                <Route exact path='/cart' element={<Cart />} />
             </Routes>
         </Router>
     );
