1438155446b7a51f3773e9eb7d809e405a3fd548
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-03-27 23:26:31 +0200

    updated

--- server-laravel/app/Http/Controllers/API/ItemController.php
+++ server-laravel/app/Http/Controllers/API/ItemController.php
@@ -3,63 +3,34 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Item;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
-    public function index1($cateName)
-    {
-        // $items = DB::table('categories')
-        // ->join('item_cates', 'categories.cate_id', '=', 'item_cates.cate_id')
-        // ->join('items', 'item_cates.item_id', '=', 'items.id')
-        // ->where('categories.cate_name', $cateName)
-        // ->get();
 
-        // $items = DB::table('items')
-        //     ->join('item_cates', 'items.id', '=', 'item_cates.item_id')
-        //     ->join('categories', 'item_cates.cate_id', '=', 'categories.cate_id')
-        //     ->select('items.title', 'items.image_link')
-        //     ->where('categories.cate_name', $cateName)
-        //     ->groupBy('items.title', 'items.image_link')
-        //     ->paginate(12);
+    public function filter(Request $request){
+        return Item::filter($request)->get();
+    }
 
-        $items = DB::table('items')
-            ->join('item_cates', 'items.id', '=', 'item_cates.item_id')
-            ->join('categories', 'item_cates.cate_id', '=', 'categories.cate_id')
-            ->where('categories.cate_name', $cateName)
-            ->get();
-
-            // $items2 = $items->groupBy('title');
+    public function index1($cateName, Request $request)
+    {   
+        $items = Item::filter($request)->whereHas('categories', function($q) use($cateName) {
+            $q->where('cate_name', $cateName);
+        })->with('parent')->with('galleries')->get();
 
         return response()->json([
             'status' => 200,
             'items' => $items,
         ]);
     }
-    public function index2($cateName, $subCateName)
+    public function index2($cateName, $subCateName, Request $request)
     {
-        // $items = DB::table('categories')
-        // ->join('item_cates', 'categories.cate_id', '=', 'item_cates.cate_id')
-        // ->join('items', 'item_cates.item_id', '=', 'items.id')
-        // ->where('categories.cate_name', $cateName)
-        // ->get();
 
-        // $items = DB::table('items')
-        //     ->join('item_cates', 'items.id', '=', 'item_cates.item_id')
-        //     ->join('categories', 'item_cates.cate_id', '=', 'categories.cate_id')
-        //     ->select('items.title', 'items.image_link')
-        //     ->where('categories.cate_name', $cateName)
-        //     ->groupBy('items.title', 'items.image_link')
-        //     ->paginate(12);
-
-        $items = DB::table('items')
-            ->join('item_cates', 'items.id', '=', 'item_cates.item_id')
-            ->join('categories', 'item_cates.cate_id', '=', 'categories.cate_id')
-            ->where('categories.cate_name', $subCateName)
-            ->get();
-
-            // $items2 = $items->groupBy('title');
+        $items = Item::filter($request)->whereHas('categories', function($q) use($subCateName) {
+            $q->where('cate_name', $subCateName);
+        })->with('parent')->with('galleries')->get();
 
         return response()->json([
             'status' => 200,
@@ -67,29 +38,11 @@ class ItemController extends Controller
         ]);
     }
 
-    public function index3($cateName, $subCateName, $subSubCateName)
+    public function index3($cateName, $subCateName, $subSubCateName, Request $request)
     {
-        // $items = DB::table('categories')
-        // ->join('item_cates', 'categories.cate_id', '=', 'item_cates.cate_id')
-        // ->join('items', 'item_cates.item_id', '=', 'items.id')
-        // ->where('categories.cate_name', $cateName)
-        // ->get();
-
-        // $items = DB::table('items')
-        //     ->join('item_cates', 'items.id', '=', 'item_cates.item_id')
-        //     ->join('categories', 'item_cates.cate_id', '=', 'categories.cate_id')
-        //     ->select('items.title', 'items.image_link')
-        //     ->where('categories.cate_name', $cateName)
-        //     ->groupBy('items.title', 'items.image_link')
-        //     ->paginate(12);
-
-        $items = DB::table('items')
-            ->join('item_cates', 'items.id', '=', 'item_cates.item_id')
-            ->join('categories', 'item_cates.cate_id', '=', 'categories.cate_id')
-            ->where('categories.cate_name', $subSubCateName)
-            ->get();
-
-            // $items2 = $items->groupBy('title');
+        $items = Item::filter($request)->whereHas('categories', function($q) use($subSubCateName) {
+            $q->where('cate_name', $subSubCateName);
+        })->with('parent')->with('galleries')->get();
 
         return response()->json([
             'status' => 200,
@@ -97,33 +50,47 @@ class ItemController extends Controller
         ]);
     }
 
-    public function index4($cateName, $subCateName, $subSubCateName, $subSubSubCateName)
+    public function index4($cateName, $subCateName, $subSubCateName, $subSubSubCateName, Request $request)
     {
-        // $items = DB::table('categories')
-        // ->join('item_cates', 'categories.cate_id', '=', 'item_cates.cate_id')
-        // ->join('items', 'item_cates.item_id', '=', 'items.id')
-        // ->where('categories.cate_name', $cateName)
-        // ->get();
-
-        // $items = DB::table('items')
-        //     ->join('item_cates', 'items.id', '=', 'item_cates.item_id')
-        //     ->join('categories', 'item_cates.cate_id', '=', 'categories.cate_id')
-        //     ->select('items.title', 'items.image_link')
-        //     ->where('categories.cate_name', $cateName)
-        //     ->groupBy('items.title', 'items.image_link')
-        //     ->paginate(12);
-
-        $items = DB::table('items')
-            ->join('item_cates', 'items.id', '=', 'item_cates.item_id')
-            ->join('categories', 'item_cates.cate_id', '=', 'categories.cate_id')
-            ->where('categories.cate_name', $subSubSubCateName)
-            ->get();
-
-            // $items2 = $items->groupBy('title');
+        $items = Item::filter($request)->whereHas('categories', function($q) use($subSubSubCateName) {
+            $q->where('cate_name', $subSubSubCateName);
+        })->with('parent')->with('galleries')->get();
 
         return response()->json([
             'status' => 200,
             'items' => $items,
+        ]);
+    }
+
+    public function item($title)
+    {
+
+        $item = Item::where('title', $title)->with('categories','galleries','parent')->first();
+
+        $like_items = Item::whereHas('categories', function($q) use($item){
+            $q->where('cate_name', $item->categories[0]->cate_name);
+        })->with('galleries', 'parent')->take(4)->get();
+
+        $same_marche = Item::whereHas('categories', function($q) use($item){
+            $q->where('cate_name', $item->categories[0]->cate_name);
+        })->where('marche', $item->marche)->with('galleries', 'parent')->get();
+
+        return response()->json([
+            'status' => 200,
+            'item' => $item,
+            'like_items' => $like_items,
+            'same_marche' => $same_marche,
+        ]);
+    }
+
+    public function specialItems(Request $request)
+    {
+        $special_items = Item::with('parent')->take(10)->get();
+        $search = Item::filter($request)->with('parent')->take(100)->get();
+        return response([
+            'status' => 200,
+            'special_items' => $special_items,
+            'search' => $search,
         ]);
     }
 }
