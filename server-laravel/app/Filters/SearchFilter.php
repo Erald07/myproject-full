b86fc8f772203860b94810886f8f5344fb9fa3cd
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-03-27 23:25:47 +0200

    filtering products by title

---
+++ server-laravel/app/Filters/SearchFilter.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Filters;
+
+class SearchFilter
+{
+    public function filter($builder, $value)
+    {
+        return $builder->where('title', 'LIKE', '%'.$value.'%');
+    }
+}
